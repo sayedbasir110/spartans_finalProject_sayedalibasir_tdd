f34dd38e0e3cd3fac7730d10cc2de343dfbf0abd
sayedbasir110 <basir.sayedali@gmail.com>
2024-09-18 23:42:57 -0700

    first commit

--- src/test/java/tek_insurance/tdd/api/models/enums/Endpoints.java
+++ src/test/java/tek_insurance/tdd/api/models/enums/Endpoints.java
@@ -1,13 +1,15 @@
 package tek_insurance.tdd.api.models.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public enum Endpoints {
     TOKEN("/api/token"),
     GET_PRIMARY_ACCOUNT("/api/accounts/get-primary-account"),
     GET_ALL_PLAN_CODE("/api/plans/get-all-plan-code"),
     ADD_PRIMARY_ACCOUNT("/api/accounts/add-primary-account"),
     GET_ACCOUNT("/api/accounts/get-account");
-
     private final String value;
-    Endpoints(String value){this.value = value;}
-    public String getValue(){return value;}
 }
